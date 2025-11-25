d52a4ef3d2a6cf5947a4769ef179a57663ea3ae2
wageehyonan <wageehyonan15@gmail.com>
2025-11-25 01:11:24 -0800

    Add files via upload

---
+++ Ecommerce_G02.Shared/ErrorModels/ValidationErrorsToReturn.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_G02.Shared.ErrorModels
+{
+    public class ValidationErrorsToReturn
+    {
+        public int StatusCode { get; set; }=(int) HttpStatusCode.BadRequest;
+        public string Message { get; set; } = "Validation Errors";
+
+        public IEnumerable<ValidationError> ValidationErrors { get; set; }
+    }
+}
