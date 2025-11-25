f31cc5eb4cecf66804f36fa53c0a8fd486cb6f5a
wageehyonan <wageehyonan15@gmail.com>
2025-11-25 01:22:47 -0800

    Add files via upload

---
+++ Ecommerce_G02.Shared/DTOs/OderDtos/OrderToReturnDto.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce_G02.Shared.DTOs.IdentityDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_G02.Shared.DTOs.OderDtos
+{
+    public class OrderToReturnDto
+    {
+        public Guid Id { get; set; }
+        public string UserEmail { get; set; } = null!;
+        public DateTimeOffset OrderDate     { get; set; }
+        public string DeliveryMethod { get; set; } = null!;
+
+        public string OrderStatus { get; set; }
+        public AddressDto Address { get; set; }
+        public ICollection<OrderItemDto> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+    }
+}
